6faedefa6351640fbe4513bfb0acd3bba537157a
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2016-11-01 11:17:19 +0000

    Amend plugin name

--- finds-org-uk-artefacts-and-coins.php
+++ finds-org-uk-artefacts-and-coins.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Finds.org.uk Artefacts and Coins
-Description: Display up-to-date artefacts and coins data on your WordPress blog.
+Description: Display up-to-date artefacts and coins data from the Portable Antiquities Scheme (finds.org.uk) on your Wordpress blog.
 Version:     1.0.0
 Author:      Mary Chester-Kadwell
 Author URI:  https://finds.org.uk/
@@ -9,3 +9,7 @@ License:     GPL3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 Text Domain: finds-org-uk
 */
+
+// Block direct access to plugin file
+defined( 'ABSPATH' ) or exit("Plugin must not be accessed directly.");
+
