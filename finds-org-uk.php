9f97b8ac0214ed6d10dc93f4eb130f050172566e
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2016-11-01 10:38:07 +0000

    Basic plugin file with header

---
+++ finds-org-uk.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Finds.org.uk Artefacts and Coins
+Description: Display up-to-date artefacts and coins data on your WordPress blog.
+Version:     1.0.0
+Author:      Mary Chester-Kadwell
+Author URI:  https://finds.org.uk/
+License:     GPL3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+Text Domain: finds-org-uk
+*/
