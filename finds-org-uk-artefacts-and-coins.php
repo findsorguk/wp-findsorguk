4a241067337222a1e1a4302fc8beb025d11c741f
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2016-11-22 17:08:31 +0000

    Load plugin CSS

--- finds-org-uk-artefacts-and-coins.php
+++ finds-org-uk-artefacts-and-coins.php
@@ -26,18 +26,31 @@ Text Domain: finds-org-uk
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-// Block direct access to plugin file
+// Blocks direct access to plugin file
 defined( 'ABSPATH' ) or exit("Plugin must not be accessed directly.");
 
-// Register a shortcode [artefact] to display an artefact record in posts
+
+// Enqueues CSS styles
+add_action( 'wp_enqueue_scripts', 'fouaac_load_style' );
+
+function fouaac_load_style() {
+    wp_register_style( 'fouaac-style', plugins_url('/css/fouaac-style.css', __FILE__) );
+    wp_enqueue_style( 'fouaac-style');
+
+}
+
+// Registers a shortcode [artefact] to display an artefact record in posts
 add_shortcode( 'artefact', 'fouaac_display_artefact' );
 
 function fouaac_display_artefact( $attr ) {
-    // Make sure the controller class is loaded
+    // Loads controller class
     require_once plugin_dir_path( __FILE__ ) . 'controllers/class-fouaac-artefact-controller.php';
-    $artefact_controller = new Fouaac_Artefact_Controller( $attr );
+    $artefact_controller = new Fouaac_Artefact_Controller( $attributes );
     $artefact_controller->display_artefact();
+
 }
+
+
 
 
 
