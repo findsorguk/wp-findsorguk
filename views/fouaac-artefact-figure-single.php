536f2f56c84203941d93034f119a88dc77c04474
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2016-11-22 17:04:41 +0000

    Create view for single artefact display

---
+++ views/fouaac-artefact-figure-single.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Template used to display a single artefact figure.
+ *
+ * @package finds-org-uk-artefacts-and-coins
+ */
+?>
+
+<figure id="" class="fouaac-figure wp-caption alignnone">
+    <img class="fouaac-size-medium size-medium"
+         src="https://finds.org.uk/images/bmorris/medium/2012%20T431.jpg"
+         alt="Early medieval square-headed brooch (Surrey County Council CC-BY 4.0">
+    <small class="fouaac-copyright">
+        Surrey County Council <a href="https://creativecommons.org/licenses/by/4.0/">CC-BY 4.0</a>
+    </small>
+    <figcaption class="fouaac-caption-text wp-caption-text">
+        Early medieval square-headed brooch
+        (<a href="https://finds.org.uk/database/artefacts/record/id/815738">BERK-31901E</a>)
+
+        <?php //test content echo $this->get_caption(); ?>
+
+    </figcaption>
+
+</figure>
+
+
