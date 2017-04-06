55b6b3cf892c9f0b39ff17c4930ca8b908035bd5
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2017-04-06 12:18:21 +0100

    Add no-conflict wrapper for jQuery in WordPress

--- plugins/tinymce-button/fouaac-shortcode-form.php
+++ plugins/tinymce-button/fouaac-shortcode-form.php
@@ -68,108 +68,117 @@ echo $shortcode_form;
     //<![CDATA[
 
     /**
-     * Insert the shortcode when the submit button is clicked
+     * No-conflict WordPress wrapper. $ can be used safely inside this function for jQuery.
      */
-    jQuery('#fouaac_shortcode_submit').click(function(){
+    jQuery(function($) {
 
-        // Count the number of words in some text (string)
-        function fouaac_count_words( text ) {
-            return text.split(/[ \t\r\n]/).length;
-        }
+        /**
+         * Insert the shortcode when the submit button is clicked
+         */
+        $('#fouaac_shortcode_submit').click(function () {
 
-        // Get the form fields
-        var values = {};
-        jQuery('#TB_ajaxContent form :input').each(function(index,field) {
-            name = '#TB_ajaxContent form #'+field.id;
-            values[jQuery(name).attr('name')] = jQuery(name).val();
-        });
+            // Count the number of words in some text (string)
+            function fouaac_count_words(text) {
+                return text.split(/[ \t\r\n]/).length;
+            }
 
-        // Check if user has entered an artefact id
-        if ( values['id'] ) {
+            // Get the form fields
+            var values = {};
+            $('#TB_ajaxContent form :input').each(function (index, field) {
+                name = '#TB_ajaxContent form #' + field.id;
+                values[$(name).attr('name')] = $(name).val();
+            });
 
-            var defaults = {'caption-option':'auto',
-                'figure-size':'medium'};
+            // Check if user has entered an artefact id
+            if (values['id']) {
 
-            // Clear the submit button and entry-type select so shortcode does not take their values
-            values['submit'] = null;
-            values['entry-type'] = null;
-            // Start shortcode text
-            var fouaac_shortcode = '[artefact';
-            // Get the attributes and values
-            for( attributes in values ) {
-                // If not empty or null
-                if( values[attributes] ) {
-                    // And the values are not the default values
-                    if( values[attributes] != defaults[attributes] ) {
-                        // If the value has more than 1 word
-                        if( fouaac_count_words( String( values[attributes] ) ) > 1 ) {
-                            // Add the key="value" pair with quotes around the value
-                            fouaac_shortcode += ' '+ attributes + '="' + values[attributes]+ '"';
-                        } else {
-                            // Otherwise just add the key=value pair
-                            fouaac_shortcode += ' ' + attributes + '=' + values[attributes];
+                var defaults = {
+                    'caption-option': 'auto',
+                    'figure-size': 'medium'
+                };
+
+                // Clear the submit button and entry-type select so shortcode does not take their values
+                values['submit'] = null;
+                values['entry-type'] = null;
+                // Start shortcode text
+                var fouaac_shortcode = '[artefact';
+                // Get the attributes and values
+                for (attributes in values) {
+                    // If not empty or null
+                    if (values[attributes]) {
+                        // And the values are not the default values
+                        if (values[attributes] != defaults[attributes]) {
+                            // If the value has more than 1 word
+                            if (fouaac_count_words(String(values[attributes])) > 1) {
+                                // Add the key="value" pair with quotes around the value
+                                fouaac_shortcode += ' ' + attributes + '="' + values[attributes] + '"';
+                            } else {
+                                // Otherwise just add the key=value pair
+                                fouaac_shortcode += ' ' + attributes + '=' + values[attributes];
+                            }
                         }
                     }
                 }
+                // End shortcode text
+                fouaac_shortcode += ']';
+                // Insert shortcode into the active editor
+                tinyMCE.activeEditor.execCommand('mceInsertContent', false, fouaac_shortcode);
+                // Close Thickbox
+                tb_remove();
+
+            } else { // No artefact id entered, send user a message
+                $("#id-explanation").html("An artefact is required!");
+                $("#id-explanation").addClass("fouaac_validation_error");
+                $("input[name='id']").addClass("fouaac_validation_error");
             }
-            // End shortcode text
-            fouaac_shortcode += ']';
-            // Insert shortcode into the active editor
-            tinyMCE.activeEditor.execCommand('mceInsertContent', false, fouaac_shortcode);
-            // Close Thickbox
-            tb_remove();
-
-        } else { // No artefact id entered, send user a message
-            jQuery( "#id-explanation" ).html( "An artefact is required!" );
-            jQuery( "#id-explanation" ).addClass( "fouaac_validation_error" );
-            jQuery( "input[name='id']" ).addClass( "fouaac_validation_error" );
-        }
 
 
-    });
+        });
 
-    /**
-     * Change text label and explanation text for artefact input to match whatever entry-type is chosen by the user
-     */
-    // If a change is detected in the entry-type selection drop down
-    jQuery( "select[id='entry-type']" ).change( function() {
-        // Get the value of the currently selected option
-        var entryOption = jQuery( "select[id='entry-type']" ).val();
-        var newLabel = '';
-        var newExplanation = '';
-        // Depending on what entry-type the user chooses, update the label, explanation and text input size
-        switch ( entryOption ) {
-        case 'url':
-                newLabel = 'Enter the full web address of the artefact record:';
-                newExplanation = 'Example: <strong>https://finds.org.uk/database/artefacts/record/id/828850</strong>';
-                jQuery('#id').attr('size', 55);
-            break;
-        case 'unique-id':
-            newLabel = 'Enter the unique ID found on the artefact\'s page:';
-            newExplanation = 'Example: <strong>IOW-647A2A</strong>';
-            jQuery('#id').attr('size', 20);
-            break;
-        case 'record-id':
-            newLabel = 'Enter the record ID found at the end of the web address:';
-            newExplanation = 'Example: for https://finds.org.uk/database/artefacts/record/id/828850 the record ID is <strong>828850</strong>';
-            jQuery('#id').attr('size', 15);
-            break;
-        default:
-        }
-        jQuery( "label[for='id']" ).text( newLabel );
-        jQuery( "#id-explanation" ).html( newExplanation );
+        /**
+         * Change text label and explanation text for artefact input to match whatever entry-type is chosen by the user
+         */
+        // If a change is detected in the entry-type selection drop down
+        $("select[id='entry-type']").change(function () {
+            // Get the value of the currently selected option
+            var entryOption = $("select[id='entry-type']").val();
+            var newLabel = '';
+            var newExplanation = '';
+            // Depending on what entry-type the user chooses, update the label, explanation and text input size
+            switch (entryOption) {
+                case 'url':
+                    newLabel = 'Enter the full web address of the artefact record:';
+                    newExplanation = 'Example: <strong>https://finds.org.uk/database/artefacts/record/id/828850</strong>';
+                    $('#id').attr('size', 55);
+                    break;
+                case 'unique-id':
+                    newLabel = 'Enter the unique ID found on the artefact\'s page:';
+                    newExplanation = 'Example: <strong>IOW-647A2A</strong>';
+                    $('#id').attr('size', 20);
+                    break;
+                case 'record-id':
+                    newLabel = 'Enter the record ID found at the end of the web address:';
+                    newExplanation = 'Example: for https://finds.org.uk/database/artefacts/record/id/828850 the record ID is <strong>828850</strong>';
+                    $('#id').attr('size', 15);
+                    break;
+                default:
+            }
+            $("label[for='id']").text(newLabel);
+            $("#id-explanation").html(newExplanation);
 
-    });
+        });
 
-    /**
-     * Toggle the visibility of the caption text input depending on whether the user chooses automatic or no caption
-     */
-    // If a change is detected in the entry-type selection dropdown
-    jQuery( "select[id='caption-option']" ).change( function() {
-        // Toggle the visibility
-        jQuery( "label[for='caption-text']" ).toggle( "fast" );
-        jQuery( "input[name='caption-text']" ).toggle( "fast" );
-        jQuery( "#caption-text-explanation" ).toggle( "fast" );
+        /**
+         * Toggle the visibility of the caption text input depending on whether the user chooses automatic or no caption
+         */
+        // If a change is detected in the entry-type selection dropdown
+        $("select[id='caption-option']").change(function () {
+            // Toggle the visibility
+            $("label[for='caption-text']").toggle("fast");
+            $("input[name='caption-text']").toggle("fast");
+            $("#caption-text-explanation").toggle("fast");
+
+        });
 
     });
 
