88fddca8f974c79e2bc1bb868a42012c2e817e3a
Mary Chester-Kadwell <mchesterkadwell@gmail.com>
2017-04-07 13:23:25 +0100

    Fix underscores/hyphens to be consistent

--- plugins/tinymce-button/fouaac-shortcode-form.js
+++ plugins/tinymce-button/fouaac-shortcode-form.js
@@ -11,7 +11,7 @@ jQuery(function ($) {
     /**
      * Insert the shortcode when the submit button is clicked
      */
-    $('#fouaac_shortcode_submit').click(function () {
+    $('#fouaac-shortcode-submit').click(function () {
 
         // Get the form fields
         var values = {};
@@ -20,21 +20,21 @@ jQuery(function ($) {
             values[$(name).attr('name')] = $(name).val();
         });
 
-        var artefactId = values.id;
-        var entryType = values.entry_type;
+        var artefactId = values['id'];
+        var entryType = values['entry-type'];
 
         // Check if user has entered a valid artefact id
         var recordId = fouaacGetValidRecordId(artefactId, entryType);
         if (recordId) {
             // Reset the id value with the new validated value
-            values.id = recordId;
-            // Clear the submit button and entry_type select so shortcode does not take their values
-            values.submit = null;
-            values.entry_type = null;
+            values['id'] = recordId;
+            // Clear the submit button and entry-type select so shortcode does not take their values
+            values['submit'] = null;
+            values['entry-type'] = null;
 
             var defaults = {
-                'caption_option': 'auto',
-                'figure_size': 'medium'
+                'caption-option': 'auto',
+                'figure-size': 'medium'
             };
 
             // Start shortcode text
@@ -68,27 +68,27 @@ jQuery(function ($) {
     });
 
     /**
-     * Change text label and explanation text for artefact input to match whatever entry_type is chosen by the user.
+     * Change text label and explanation text for artefact input to match whatever entry-type is chosen by the user.
      */
-    // If a change is detected in the entry_type selection drop down
-    $("select[id='entry_type']").change(function () {
+    // If a change is detected in the entry-type selection drop down
+    $("select[id='entry-type']").change(function () {
         // Get the value of the currently selected option
-        var entryType = $("select[id='entry_type']").val();
+        var entryType = $("select[id='entry-type']").val();
         var newLabel = '';
         var newExplanation = '';
-        // Depending on what entry_type the user chooses, update the label, explanation and text input size
+        // Depending on what entry-type the user chooses, update the label, explanation and text input size
         switch (entryType) {
             case 'url':
                 newLabel = 'Enter the full web address of the artefact record:';
                 newExplanation = 'Example: <strong>https://finds.org.uk/database/artefacts/record/id/828850</strong>';
                 $('#id').attr('size', 55);
                 break;
-            case 'unique_id':
+            case 'unique-id':
                 newLabel = 'Enter the unique ID found on the artefact\'s page:';
                 newExplanation = 'Example: <strong>IOW-647A2A</strong>';
                 $('#id').attr('size', 20);
                 break;
-            case 'record_id':
+            case 'record-id':
                 newLabel = 'Enter the record ID found at the end of the web address:';
                 newExplanation = 'Example: for https://finds.org.uk/database/artefacts/record/id/828850 the record ID is <strong>828850</strong>';
                 $('#id').attr('size', 15);
@@ -96,19 +96,19 @@ jQuery(function ($) {
             default:
         }
         $("label[for='id']").text(newLabel);
-        $("#id_explanation").html(newExplanation);
+        $("#id-explanation").html(newExplanation);
 
     });
 
     /**
      * Toggle the visibility of the caption text input depending on whether the user chooses automatic or no caption.
      */
-    // If a change is detected in the entry_type selection dropdown
-    $("select[id='caption_option']").change(function () {
+    // If a change is detected in the entry-type selection dropdown
+    $("select[id='caption-option']").change(function () {
         // Toggle the visibility
-        $("label[for='caption_text']").toggle("fast");
-        $("input[name='caption_text']").toggle("fast");
-        $("#caption_text_explanation").toggle("fast");
+        $("label[for='caption-text']").toggle("fast");
+        $("input[name='caption-text']").toggle("fast");
+        $("#caption-text-explanation").toggle("fast");
 
     });
 
@@ -139,14 +139,14 @@ jQuery(function ($) {
                         fouaacDisplayErrorMessage("Please check your URL and try again.");
                     }
                     break;
-                case 'unique_id':
+                case 'unique-id':
                     recordId = fouaacLookupRecordId(artefactId);
                     if (!recordId) {
                         fouaacDisplayErrorMessage("Your unique ID does not match an artefact record on public display. " +
                             "Please check and try again.");
                     }
                     break;
-                case 'record_id':
+                case 'record-id':
                     recordId = artefactId.trim();
                     if (!fouaacIsInt1To999999(recordId)) {
                         fouaacDisplayErrorMessage("Please check your record ID and try again.");
@@ -217,9 +217,9 @@ jQuery(function ($) {
      * @return {undefined}
      */
     function fouaacDisplayErrorMessage(message) {
-        $("#id_explanation").html(message);
-        $("#id_explanation").addClass("fouaac_validation_error");
-        $("input[name='id']").addClass("fouaac_validation_error");
+        $("#id-explanation").html(message);
+        $("#id-explanation").addClass("fouaac-validation-error");
+        $("input[name='id']").addClass("fouaac-validation-error");
     }
 
     /**
@@ -235,7 +235,7 @@ jQuery(function ($) {
     /**
      * Check if a value is an integer between 1 and 999999.
      *
-     * @param {mixed} val Value to check.
+     * @param {string} val Value to check.
      * @return {boolean} True if 1-999999, otherwise false.
      */
     function fouaacIsInt1To999999(val) {
